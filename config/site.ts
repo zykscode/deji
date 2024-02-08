eaeddf965fefd8a68751947bb2ebc7e0f4f3c244
Isaac <zyskson@gmail.com>
2024-02-08 18:16:17 +0100

    here okay 3

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,12 @@
-import type { SiteConfig } from '@/types';
+import type { SiteConfig } from '#/types';
 
 export const siteConfig: SiteConfig = {
-  name: "Zykson",
+  name: 'Zykson',
   description: 'A personal blog and portfolio site',
   url: 'https://tx.shadcn.com',
   ogImage: 'https://tx.shadcn.com/og.jpg',
   links: {
     twitter: 'zykson',
     github: 'zykscode',
-    youtube: 'zykson',
   },
 };
